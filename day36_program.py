fe43c9ec4e031c5d5146a891faf845a7241f5a1d
dikshabarewar6-collab <dikshabarewar6@gmail.com>
2026-04-01 07:10:25 +0530

    Add files via upload

---
+++ day36_program.py
@@ -0,0 +1,16 @@
+# Quick Sort
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+
+    left = [x for x in arr[1:] if x <= pivot]
+    right = [x for x in arr[1:] if x > pivot]
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+arr = [5, 2, 8, 1]
+print(quick_sort(arr))
