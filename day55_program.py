5808b53e129185f9864dd823e0f82ddc3671b1c0
dikshabarewar6-collab <dikshabarewar6@gmail.com>
2026-04-20 08:56:51 +0530

    day55_program.py

---
+++ day55_program.py
@@ -0,0 +1,31 @@
+# Dynamic Programming
+memo = {}
+
+def fib(n):
+    if n in memo:
+        return memo[n]
+
+    if n <= 1:
+        return n
+
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+
+print(fib(6))
+
+
+
+
+# Tabulation
+
+def fib(n):
+    dp = [0] * (n+1)
+
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n]
+
+print(fib(6))
